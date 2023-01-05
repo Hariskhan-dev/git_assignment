9ebf6dcf8e32e2e17927523949f22e7d71f6e6ff
Aneed khan <115465921+aneed786@users.noreply.github.com>
2023-01-05 12:02:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 let n = 13;
 let count = 0;
 for(let i=1; i<=n; i++){
-  if(n%i==1){
+  if(n%i==0){
     count++;
   }
 }
 
 if(count == 2){
-  conosl.log(n+' '+'is a prime number');
+  conosl.log("it is a prime number");
 } else{
-  console.log(n+' '+'is not a prime number');
+  console.log("it is not a prime number');
 }
 
 
-let str = 'racecar';
-let revStr = '';
+let str = "racecar";
+let rev = '';
 for(let i=str.length-1; i<0; i--){
-  revStr+=str[i];
+  rev+=str[i];
 }
-console.log(revStr);
-if(revStr == str){
+
+if(rev == str){
   console.log("Palindrom");
 } else {
-  console.log("Not palindrome")
+  console.log("Not a Palindrome")
 }
 
 
